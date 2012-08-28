facdbb6dd356aa3df5f0009a3d5743266867019e
Erich Menge <erich.menge@me.com>
2012-08-28 12:14:06 -0500

    Move include out of Engine.

--- lib/api-versions.rb
+++ lib/api-versions.rb
@@ -1,42 +1,35 @@
 require "api-versions/version"
 
-class Engine < Rails::Engine
-  initializer "api_versions" do
-    config.to_prepare { ActionDispatch::Routing::Mapper.send :include, ApiVersions }
-  end
-end
-
 module ApiVersions
-
   def inherit_resources(args)
     [*args[:from]].each do |block|
       @resource_cache[block].call
     end
   end
-    
+
   def cache_resources(args, &block)
     @resource_cache ||= {}
     @resource_cache.merge!(args[:as] => block)
     block.call
   end
-  
+
   def api_version=(version)
     ApiVersions::ApiVersionCheck.api_version = version
   end
   alias_method :default_api_version, :api_version=
-  
-  
+
+
   def vendor=(vendor)
     ApiVersions::ApiVersionCheck.api_vendor = vendor
   end
   alias_method :api_vendor, :vendor=
-  
+
   def api_version_check(*args)
     ApiVersions::ApiVersionCheck.new(*args)
   end
-  
+
   class ApiVersionCheck
-    
+
     cattr_accessor :api_version, :api_vendor
 
     def initialize(args = {})
@@ -46,7 +39,7 @@ module ApiVersions
     def matches?(request)
       accepts_proper_format?(request) && (matches_version?(request) || unversioned?(request))
     end
-    
+
     private
 
     def accepts_proper_format?(request)
@@ -60,6 +53,7 @@ module ApiVersions
     def unversioned?(request)
         @process_version == self.class.api_version && !(request.headers['Accept'] =~ /version\s*?=\s*?\d*\b/i)
     end
-    
   end
 end
+
+ActionDispatch::Routing::Mapper.send :include, ApiVersions
