eb6eb9a4b62393bd852d67aed66f06ce48caf343
Erich Menge <erich.menge@me.com>
2012-08-29 21:58:55 -0500

    Split module apart into separate files.

---
+++ lib/api-versions/dsl.rb
@@ -0,0 +1,31 @@
+module ApiVersions
+  class DSL
+    extend Forwardable
+
+    def initialize(context, &block)
+      @context = context
+      self.class.def_delegators :@context, *(@context.public_methods - public_methods)
+      instance_eval &block
+    end
+
+    def version(version_number, &block)
+      VersionCheck.default_version ||= version_number
+
+      constraints VersionCheck.new(version: version_number) do
+        scope({ module: "v#{version_number}" }, &block)
+      end
+    end
+
+    def inherit(args)
+      [*args[:from]].each do |block|
+         @resource_cache[block].call
+      end
+    end
+
+    def cache(args, &block)
+      @resource_cache ||= {}
+      @resource_cache[args[:as]] = block
+      block.call
+    end
+  end
+end
