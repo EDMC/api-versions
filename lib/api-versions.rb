d134d8925a52d1cf9bb1de61dae8de8dff27b39b
Erich Menge <erich.menge@me.com>
2012-08-29 21:58:55 -0500

    Split module apart into separate files.

--- lib/api-versions.rb
+++ lib/api-versions.rb
@@ -1,62 +1,8 @@
 require "api-versions/version"
+require "api-versions/version_check"
+require "api-versions/dsl"
 
 module ApiVersions
-  class DSL
-    extend Forwardable
-
-    def initialize(context, &block)
-      @context = context
-      self.class.def_delegators :@context, *(@context.public_methods - public_methods)
-      instance_eval &block
-    end
-
-    def version(version_number, &block)
-      VersionCheck.default_version ||= version_number
-
-      constraints VersionCheck.new(version: version_number) do
-        scope({ module: "v#{version_number}" }, &block)
-      end
-    end
-
-    def inherit(args)
-      [*args[:from]].each do |block|
-         @resource_cache[block].call
-      end
-    end
-
-    def cache(args, &block)
-      @resource_cache ||= {}
-      @resource_cache[args[:as]] = block
-      block.call
-    end
-  end
-
-  class VersionCheck
-    cattr_accessor :default_version, :vendor_string
-
-    def initialize(args = {})
-      @process_version = args[:version]
-    end
-
-    def matches?(request)
-      accepts_proper_format?(request) && (matches_version?(request) || unversioned?(request))
-    end
-
-    private
-
-    def accepts_proper_format?(request)
-      !!(request.headers['Accept'] =~ /^application\/vnd\.#{self.class.vendor_string}\+.+/)
-    end
-
-    def matches_version?(request)
-      !!(request.headers['Accept'] =~ /version\s*?=\s*?#{@process_version}\b/)
-    end
-
-    def unversioned?(request)
-        @process_version == self.class.default_version && !(request.headers['Accept'] =~ /version\s*?=\s*?\d*\b/i)
-    end
-  end
-
   def api(options = {}, &block)
     VersionCheck.default_version = options[:default_version]
     VersionCheck.vendor_string   = options[:vendor_string]
---
+++ lib/api-versions/version_check.rb
@@ -0,0 +1,27 @@
+module ApiVersions
+  class VersionCheck
+    cattr_accessor :default_version, :vendor_string
+
+    def initialize(args = {})
+      @process_version = args[:version]
+    end
+
+    def matches?(request)
+      accepts_proper_format?(request) && (matches_version?(request) || unversioned?(request))
+    end
+
+    private
+
+    def accepts_proper_format?(request)
+      !!(request.headers['Accept'] =~ /^application\/vnd\.#{self.class.vendor_string}\+.+/)
+    end
+
+    def matches_version?(request)
+      !!(request.headers['Accept'] =~ /version\s*?=\s*?#{@process_version}\b/)
+    end
+
+    def unversioned?(request)
+        @process_version == self.class.default_version && !(request.headers['Accept'] =~ /version\s*?=\s*?\d*\b/i)
+    end
+  end
+end
