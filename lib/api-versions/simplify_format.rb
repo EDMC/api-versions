23a93a41b07b8542baab2d0ed9a701ae21cd0c53
Erich Menge <erich.menge@me.com>
2012-09-02 01:02:05 -0500

    Create a module for controllers to simplify the Accept header.

---
+++ lib/api-versions/simplify_format.rb
@@ -0,0 +1,15 @@
+module ApiVersions
+  module SimplifyFormat
+    extend ActiveSupport::Concern
+
+    included do
+      before_filter :simplify_format
+    end
+
+    private
+
+    def simplify_format
+      request.headers['Accept'] && request.headers['Accept'].match(/\+\s*(.+?)[;\s\z]/) { |m| request.format = m[1] }
+    end
+  end
+end
