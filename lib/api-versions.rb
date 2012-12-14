552f5311b7744c4badc157220c86b94f2dc8bec4
Erich Menge <erich.menge@me.com>
2012-12-14 14:44:42 -0600

    Raise before all else.

--- lib/api-versions.rb
+++ lib/api-versions.rb
@@ -6,10 +6,10 @@ require "api-versions/simplify_format"
 
 module ApiVersions
   def api(options = {}, &block)
+    raise "Please set a vendor_string for the api method" if options[:vendor_string].nil?
+
     VersionCheck.default_version = options[:default_version]
     VersionCheck.vendor_string   = options[:vendor_string]
-
-    raise "Please set a vendor_string for the api method" if options[:vendor_string].nil?
 
     namespace(:api) { DSL.new(self, &block) }
   end
