56f9045034853c65865684e6eeb9203e9c6dbe36
Erich Menge <erich.menge@me.com>
2013-05-10 18:39:49 -0500

    Minor regex tweak.

--- lib/api-versions/middleware.rb
+++ lib/api-versions/middleware.rb
@@ -9,7 +9,7 @@ module ApiVersions
       offset = 0
       accepts.dup.each_with_index do |accept, i|
         accept.strip!
-        match = /\Aapplication\/vnd\.#{ApiVersions::VersionCheck.vendor_string}\s*\+\W*(?<format>\w+)\W*/.match(accept)
+        match = /\Aapplication\/vnd\.#{ApiVersions::VersionCheck.vendor_string}\s*\+\s*(?<format>\w+)\s*/.match(accept)
         if match
           accepts.insert i + offset, "application/#{match[:format]}"
           offset += 1
