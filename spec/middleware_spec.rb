8cd286209de7331afd35f423e04aa01b5e190f60
Vasiliy Ermolovich <younash@gmail.com>
2018-07-10 22:34:29 +0300

    Update RSpec syntax.

--- spec/middleware_spec.rb
+++ spec/middleware_spec.rb
@@ -15,25 +15,25 @@ describe ApiVersions::Middleware do
     it "should not adjust the header" do
       request = Rack::MockRequest.env_for("/", "HTTP_ACCEPT" => "application/vnd.foobar+json;version=1", lint: true, fatal: true)
       response = described_class.new(app).call(request).last
-      response.first.should == "application/vnd.foobar+json;version=1"
+      expect(response.first).to eq("application/vnd.foobar+json;version=1")
     end
 
     it "should adjust the header" do
       request = Rack::MockRequest.env_for("/", "HTTP_ACCEPT" => "text/plain,application/vnd.myvendor+json;version=1,text/html,application/vnd.myvendor+xml", lint: true, fatal: true)
       response = described_class.new(app).call(request).last
-      response.last.should == "text/plain,application/json,application/vnd.myvendor+json;version=1,text/html,application/xml,application/vnd.myvendor+xml"
+      expect(response.last).to eq("text/plain,application/json,application/vnd.myvendor+json;version=1,text/html,application/xml,application/vnd.myvendor+xml")
     end
 
     it "should add a default vendor accept to a nil Accept header" do
       request = Rack::MockRequest.env_for("/", lint: true, fatal: true)
       response = described_class.new(app).call(request).last
-      response.last.should == "application/json,application/vnd.#{ApiVersions::VersionCheck.vendor_string}+json"
+      expect(response.last).to eq("application/json,application/vnd.#{ApiVersions::VersionCheck.vendor_string}+json")
     end
 
     it "should add a default vendor accept to an empty Accept header" do
       request = Rack::MockRequest.env_for("/", "HTTP_ACCEPT" => '', lint: true, fatal: true)
       response = described_class.new(app).call(request).last
-      response.last.should == "application/json,application/vnd.#{ApiVersions::VersionCheck.vendor_string}+json"
+      expect(response.last).to eq("application/json,application/vnd.#{ApiVersions::VersionCheck.vendor_string}+json")
     end
   end
 end
